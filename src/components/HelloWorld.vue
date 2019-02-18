03b8db188ef47693b4bf6a0fee2f0028f5089467
Juan Pablo Balda <jpbalda@hotmail.com>
2019-02-17 19:34:08 -0500

    8 - Vue.js with a Theme and Scoped CSS Styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>Welcome to the Forum</h1>
-    <div v-for="thread in threads">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts">
-        <p>{{ users[posts[postId].userId].name }}</p>
-        <p>{{ posts[postId].text }}</p>
+    <div v-for="thread in threads" class="col-large push-top">
+      <h1>{{ thread.title }}</h1>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
+
+            <a href="#">
+              <img :src="users[posts[postId].userId].avatar" alt class="avatar-large">
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>{{ posts[postId].text }}</div>
+          </div>
+
+          <div class="post-date text-faded">{{ posts[postId].publishedAt }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +35,7 @@ export default {
       threads: sourceData.threads,
       posts: sourceData.posts,
       users: sourceData.users
-    }
+    };
   }
 };
 </script>
