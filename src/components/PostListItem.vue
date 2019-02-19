d665593c748f6ec8fa98c27365a0132562cfb244
Juan Pablo Balda <jpbalda@hotmail.com>
2019-02-18 20:06:36 -0500

    20 - Human Friendly Dates

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,27 +1,52 @@
 <template>
   <div class="post">
     <div class="user-info">
-      <a href="#" class="user-name">{{ user.name }}</a>
-
-      <a href="#">
-        <img :src="user.avatar" alt class="avatar-large">
+      <a
+        href="#"
+        class="user-name"
+      >
+        {{ user.name }}
       </a>
 
-      <p class="desktop-only text-small">{{ userPostCount }} posts</p>
+      <a href="#">
+        <img
+          :src="user.avatar"
+          alt
+          class="avatar-large"
+        >
+      </a>
+
+      <p class="desktop-only text-small">
+        {{ userPostCount }} posts
+      </p>
     </div>
 
     <div class="post-content">
       <div>{{ post.text }}</div>
     </div>
 
-    <div class="post-date text-faded">{{ post.publishedAt }}</div>
+    <div
+      class="post-date text-faded"
+      :title="post.publishedAt | humanFriendlyDate"
+    >
+      {{ post.publishedAt | diffForHumans }}
+    </div>
   </div>
 </template>
 
 <script>
-import sourceData from "@/data";
+import moment from 'moment'
+import sourceData from '@/data'
 
 export default {
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
+    }
+  },
   props: {
     post: {
       required: true,
@@ -29,12 +54,12 @@ export default {
     }
   },
   computed: {
-    user() {
-      return sourceData.users[this.post.userId];
+    user () {
+      return sourceData.users[this.post.userId]
     },
-    userPostCount() {
-      return Object.keys(this.user.posts).length;
+    userPostCount () {
+      return Object.keys(this.user.posts).length
     }
   }
-};
+}
 </script>
