d8c6ac0424400fce3fa20f789aa030581c6139bd
Juan Pablo Balda <jpbalda@hotmail.com>
2019-03-02 20:30:09 -0500

    24 - Forum Components

---
+++ src/components/ForumList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <a href="#">Forums</a>
+    </h2>
+    <ForumListItem
+      v-for="forum in forums"
+      :key="forum['.key']"
+      :forum="forum"
+    />
+  </div>
+</template>
+
+<script>
+import ForumListItem from './ForumListItem'
+
+export default {
+  components: {
+    ForumListItem
+  },
+  props: {
+    forums: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
