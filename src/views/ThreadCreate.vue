c64815034a2efe551fd7ddd0389fbc0ff9816568
Juan Pablo Balda <jpbalda@hotmail.com>
2019-03-03 18:49:15 -0500

    36 - Create New Threads

---
+++ src/views/ThreadCreate.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="col-full push-top">
+    <h1>Create new thread in <i>{{ forum.name }}</i></h1>
+
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input
+          id="thread_title"
+          v-model="title"
+          type="text"
+          class="form-input"
+          name="title"
+        >
+      </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          v-model="text"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+        />
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">
+          Cancel
+        </button>
+        <button
+          class="btn btn-blue"
+          type="submit"
+          name="Publish"
+        >
+          Publish
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  data () {
+    return {
+      title: '',
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      // dispatch action
+    }
+  }
+}
+</script>
+
+<style>
+</style>
